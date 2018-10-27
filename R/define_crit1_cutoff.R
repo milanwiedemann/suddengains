12f55a2fba97bc058b261e39467b8e8665fe1eae
Milan Wiedemann <milan.wiedemann@gmail.com>
2018-10-27 10:40:20 +0100

    make function work with different variable names, not only pds

--- R/define_crit1_cutoff.R
+++ R/define_crit1_cutoff.R
@@ -1,39 +1,45 @@
-#' Define the Cut-off for SG Criterion 1 using the RCI Formula
+#' Define the Cut-off for SG Criterion 1 using modified RCI Formula
 #'
 #' @param data_sessions A dataset in wide format with values for pre _s0 and post treatment _end
 #' @param data_item A dataset in item-by-item scores for the SG measure
-
-#' @return A list with all calculated variables using formulate for RCI including cut-off
+#' @param pre_var_name String, variable name of the pretreatment scores
+#' @param post_var_name String, variable name of the postreatment scores
+#' @return A list with all calculated variables using modified formula for RCI including cut-off
 #' @export
 
-define_crit1_cutoff <- function(data_sessions, data_item) {
-  # Calculate mean pds change score
-  mean_change <- base::mean(data_sessions$pds_s0, na.rm = TRUE) - base::mean(data_sessions$pds_end, na.rm = TRUE)
+define_crit1_cutoff <- function(data_sessions, data_item, pre_var_name, post_var_name) {
 
-  # Calculate RCI for PDS ----
+  # Create vectors of variables used in this function
+  pre_scores <- dplyr::pull(data_sessions, pre_var_name)
+  post_scores <- dplyr::pull(data_sessions, post_var_name)
+
+  # Calculate mean pds change score
+  mean_change_score <- base::mean(pre_scores, na.rm = TRUE) - base::mean(post_scores, na.rm = TRUE)
+
+  # Calculate RCI ----
   # Calculate the standard deviation of the sum scores at baseline
 
   # What is the start of treatment value here
-  sd <- stats::sd(data_sessions$pds_s0, na.rm = TRUE)
+  standard_deviation_pre <- stats::sd(pre_scores, na.rm = TRUE)
 
-  # Calculate Cronbach's alpha for PDS at baseline using psych package
-  alpha <- psych::alpha(data_item)[[1]]$raw_alpha
+  # Calculate Cronbach's alpha at baseline using psych package
+  alpha_pre <- psych::alpha(data_item)[[1]]$raw_alpha
 
   # Calculate standard error of measurement
-  se <- sd * base::sqrt(1 - alpha)
+  standard_error_measurement <- standard_deviation_pre * base::sqrt(1 - alpha_pre)
 
   # Calculate the standard error of the differences between two test scores
-  sdiff <- base::sqrt(2 * (se ^ 2))
+  sdiff <- base::sqrt(2 * (standard_error_measurement ^ 2))
 
-  # Calculate RCI using mean change pds
-  crit1_cutoff <- (mean_change / sdiff) * 1.96
+  # Calculate RCI using mean change
+  crit1_cutoff <- (mean_change_score / sdiff) * 1.96
 
-  output <- list(mean_change_score = mean_change,
-                 standard_deviation_at_pretreatment = sd,
-                 cronbachs_alpha_at_pretreatment = alpha,
-                 standard_error_of_measurement = se,
+  output <- list(mean_change_score = mean_change_score,
+                 standard_deviation_pre = standard_deviation_pre,
+                 alpha_pre = alpha_pre,
+                 standard_error_measurement = standard_error_measurement,
                  sdiff = sdiff,
                  crit1_cutoff = crit1_cutoff)
 
-  print(output)
+  output
 }

de1f8ae963aef998184db1a53440651052cd95e8
Milan Wiedemann <milan.wiedemann@gmail.com>
2018-12-13 21:23:39 +0000

    add data, only one measure atm, also need to add some other measures later

---
+++ R/sgdata.R
@@ -0,0 +1,21 @@
+#' Example dataset with BDI scores to illustrate how the package works
+#'
+#' Generated dataset with made up BDI scores to illustrate how the package works
+#'
+#' @docType data
+#'
+#' @usage data(sgdata)
+#'
+#' @format An longitudinal dataset in the wide format.
+#'
+#' @keywords dataset
+#'
+#' @references Moore et al. (2013) Genetics 195:1077-1086
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#'
+#' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
+#'
+#' @examples
+#' # Load data into global environment
+#' data(sgdata)
+"sgdata"

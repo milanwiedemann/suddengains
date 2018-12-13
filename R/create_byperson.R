4a7ab4718fe709c65ede3208b0bfaacfce02029d
Milan Wiedemann <milan.wiedemann@gmail.com>
2018-12-13 21:48:15 +0000

    simplify code and add more options for selecting gains!

--- R/create_byperson.R
+++ R/create_byperson.R
@@ -2,38 +2,67 @@
 #'
 #' @param data A dataset in wide format with an id variable and the sudden gains variables.
 #' @param bysg_data A \code{bysg} dataset.
-#' @param multiple_sg_select String indicating whether to select the \code{earliest} or \code{largest} sudden gain if multiple gains per person.
+#' @param multiple_sg_select String indicating whether to select the \code{earliest}, \code{latest} or \code{largest}  \code{smallest} sudden gain if multiple gains per person. when two sudden gains are the same size and smalles or largest is selcted then the first gain of the will be selected using a secon filter command
 
 #' @return A full dataset with all participants, the earliest gain per person
 #' @export
 
-create_byperson <- function(data, bysg_data, multiple_sg_select) {
+create_byperson <- function(data, cutoff, id_var_name, sg_var_list, tx_start_var_name, tx_end_var_name, sg_var_name, multiple_sg_select = "first", identify_sg_1to2 = FALSE, include_s0_extract = FALSE) {
 
-  # TODO ADD IF ELSE TO ADD OPTION FOR SELECTING "EARLIEST" OR "LARGEST" GAINS
+  data_bysg <- create_bysg(data = data,
+                           cutoff = cutoff,
+                           id_var_name = id_var_name,
+                           tx_start_var_name = tx_start_var_name,
+                           tx_end_var_name = tx_end_var_name,
+                           sg_var_list = sg_var_list,
+                           sg_var_name = sg_var_name,
+                           identify_sg_1to2 = identify_sg_1to2,
+                           include_s0_extract = include_s0_extract)
 
-data_byperson_sgonly <- bysg_data %>%
-  dplyr::group_by(id) %>%
-  dplyr::rename(sg_reversal_byperson = sg_reversal) %>%
-  dplyr::mutate(sg_reversal_byperson = ifelse(sg_reversal_byperson > 0, 1, 0)) %>%
-  dplyr::filter(sg_session_n == min(sg_session_n)) %>%
-  dplyr::group_by(id) %>%
-  dplyr::filter(sg_session_n == min(sg_session_n)) %>%
-  dplyr::ungroup() %>%
-  dplyr::select(id, id_sg, sg_crit123, sg_freq_byperson, sg_session_n,
-                # TODO change this part so not dependent on variable names (pds)
-         sg_pds_pre3, sg_pds_pre2, sg_pds_pre1, sg_pds_post1, sg_pds_post2, sg_pds_post3,
-         sg_magnitude, pds_change_total, sg_change_proportion, sg_reversal_value, sg_reversal_byperson)
 
-data_byperson <- data %>%
-  dplyr::left_join(data_byperson_sgonly)
 
-data_byperson$sg_crit123[is.na(data_byperson$sg_crit123)] <- 0
+  if (multiple_sg_select == "first") {
 
-data_byperson <- data_byperson %>%
-  dplyr::select(id, id_sg, sg_crit123, sg_freq_byperson, sg_session_n,
-                # TODO change this part so not dependent on variable names (pds)
-         sg_pds_pre3, sg_pds_pre2, sg_pds_pre1, sg_pds_post1, sg_pds_post2, sg_pds_post3,
-         sg_magnitude, pds_change_total, sg_change_proportion, sg_reversal_value, sg_reversal_byperson)
+    bysg_data_select <- data_bysg %>%
+      dplyr::select(id_var_name, id_sg, starts_with("sg_")) %>%
+      dplyr::group_by(!! rlang::sym(id_var_name)) %>%
+      dplyr::filter(sg_session_n == min(sg_session_n)) %>%
+      dplyr::ungroup()
 
-data_byperson
-}
+    } else if (multiple_sg_select == "last") {
+
+      bysg_data_select <- data_bysg %>%
+        dplyr::select(id_var_name, id_sg, starts_with("sg_")) %>%
+        dplyr::group_by(!! rlang::sym(id_var_name)) %>%
+        dplyr::filter(sg_session_n == max(sg_session_n)) %>%
+        dplyr::ungroup()
+
+      } else if (multiple_sg_select == "smallest") {
+
+        bysg_data_select <- data_bysg %>%
+          dplyr::select(id_var_name, id_sg, starts_with("sg_")) %>%
+          dplyr::group_by(!! rlang::sym(id_var_name)) %>%
+          dplyr::filter(sg_magnitude == min(sg_magnitude)) %>%
+          dplyr::filter(sg_session_n == min(sg_session_n)) %>%
+          dplyr::ungroup()
+
+        } else if (multiple_sg_select == "largest") {
+
+          bysg_data_select <- data_bysg %>%
+            dplyr::select(id_var_name, id_sg, starts_with("sg_")) %>%
+            dplyr::group_by(!! rlang::sym(id_var_name)) %>%
+            dplyr::filter(sg_magnitude == max(sg_magnitude)) %>%
+            dplyr::filter(sg_session_n == min(sg_session_n)) %>%
+            dplyr::ungroup()
+
+          }
+
+
+  # Join dataset
+  data_byperson <- bysg_data_select %>%
+    dplyr::full_join(data, by = id_var_name) %>%
+    tidyr::replace_na(list(sg_crit123 = 0, sg_freq_byperson = 0))
+
+  # Return dataset
+  data_byperson
+  }

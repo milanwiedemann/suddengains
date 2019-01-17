a3b04fa61ba99929ee2ac8946128af72c57426e6
Milan Wiedemann <milan.wiedemann@gmail.com>
2019-01-17 13:35:12 +0000

    finally fixed identify sudden loss function

--- R/identify_sl.R
+++ R/identify_sl.R
@@ -1,152 +1,164 @@
-#' @title Identify sudden loss in a wide dataset.
+#' @title Identify sudden losses in a wide dataset.
 #'
-#' @description Function identify sudden losses in a wide dataset. automatically checks whether first session gains are calculated by analysing the variable names looking for session with 0 (intake) before a letter.
+#' @description Function identify sudden losses in a wide dataset. automatically checks whether first session losses are calculated by analysing the variable names looking for session with 0 (intake) before a letter.
 #'
-#' @param data A dataset in wide format with an id variable and the sudden gains variables.
-#' @param cutoff A number specifying the cut-off for criterion 1. Negative value for identifying sudden losses.
+#' @param data A dataset in wide format with an id variable and the sudden losses variables.
 #' @param id_var_name A string of the id variable name.
-#' @param sl_var_name A string of the sudden gains variable name.
-#' @param identify_sl_1to2 Logical value to indicate whether first session gains should be in dataset, works only if s0 variable is provided, default is TRUE
+#' @param sg_var_list A LIST of the variable names for sudden losses analysis, if first session losses should be analysed simply add session 0 at the beginning of the list.
+#' @param sg_crit1_cutoff A number specifying the cut-off for criterion 1.
+#' @param sg_crit2_pct Numeric, percentage of drop second criteria sudden losses.
+#' @param identify_sg_1to2 Logical value to indicate whether first session losses should be in dataset,
+#' simply include s0 before before all the other variables in sg_var_list, it will simply change the way
+#' variables are named but this is super important for calculating which session sudden gain is in and also for extracting stuff later.
+#' @param crit123_details Logical, if crit123_details = TRUE a dataframe will be written to global environment.
+#' @param name_crit123_details String, if crit123_details = TRUE a dataframe will with the name of this argument will be written to the global environment.
 
 
 #' @return A wide dataset indicating at which between session interval a sudden gain occured for each person in \code{data}.
 #' @export
 
-identify_sl <- function(data, cutoff, id_var_name, sl_var_name, identify_sl_1to2 = FALSE) {
+identify_sl <-
+    function(data,
+             id_var_name,
+             sg_var_list,
+             sg_crit1_cutoff,
+             sg_crit2_pct,
+             identify_sg_1to2 = FALSE,
+             crit123_details = FALSE,
+             name_crit123_details = "data_crit123_details") {
 
-  # Load data ----
-  # data object is the dataframe with id as a unique identifier and all session to session variables
-  # df object is the dataframe without id that will go into the loop
+        # Select data for identifying sudden losses
+        # Only ID variable and sudden losses variables needed
+        data_select <- data %>%
+            dplyr::arrange(!!rlang::sym(id_var_name)) %>%
+            dplyr::select(!!rlang::sym(id_var_name), sg_var_list)
 
-  # Order by id here because after loop id will be binded back together by using base::cbind()
-  data <- data %>%
-    dplyr::arrange(!!rlang::sym(id_var_name)) %>%
-    dplyr::select(!!rlang::sym(id_var_name), dplyr::starts_with(sl_var_name))
+        # Remove ID from dataframe for loop
+        data_loop <- data_select %>%
+            dplyr::arrange(!!rlang::sym(id_var_name)) %>%
+            dplyr::select(2:base::ncol(data_select))
 
+        # Create one empty dataframe for each sudden losses criterion
+        crit1 <- base::data.frame(base::matrix(
+            NA,
+            nrow = base::nrow(data_loop),
+            ncol = base::ncol(data_loop) - 3
+        ))
+        crit2 <- base::data.frame(base::matrix(
+            NA,
+            nrow = base::nrow(data_loop),
+            ncol = base::ncol(data_loop) - 3
+        ))
+        crit3 <- base::data.frame(base::matrix(
+            NA,
+            nrow = base::nrow(data_loop),
+            ncol = base::ncol(data_loop) - 3
+        ))
 
-  # identify_sl <- function(data)
-  # Order by id here for safety as well because after loop id will be binded back together by using base::cbind()
-  df <- data %>%
-    dplyr::arrange(!!rlang::sym(id_var_name)) %>%
-    dplyr::select(2:base::ncol(data)) # Remove id from dataframe for loop
+        # Iterate through all rows
+        for (row_i in 1:base::nrow(data_loop)) {
 
-  # Create one empty data frame for each sudden gains criterion ----
-  # Data will be written in these in the for loop
-  crit1 <- base::data.frame(base::matrix(NA, nrow = base::nrow(df), ncol = base::ncol(df) - 3))
-  crit2 <- base::data.frame(base::matrix(NA, nrow = base::nrow(df), ncol = base::ncol(df) - 3))
-  crit3 <- base::data.frame(base::matrix(NA, nrow = base::nrow(df), ncol = base::ncol(df) - 3))
+            # Iterate through all columns
+            for (col_j in 3:(base::ncol(data_loop) - 1)) {
 
-  # Start iterating through dataframe ----
-  for (i in 1:base::nrow(df)) {
-    # Iterate through all rows
-    # print(base::paste("---------- ROW", i, "----------"))
-    for (j in 3:(base::ncol(df) - 1)) {
-      # Iterate through all columns
+                # Check 1st sudden losses criterion
+                crit1[row_i, col_j - 2] <- (data_loop[row_i, col_j - 1] - data_loop[row_i, col_j] <= sg_crit1_cutoff)
 
-      # Calculate 1st sudden gains criterion
-      crit1[i, j - 2] <- (df[i, j - 1] - df[i, j] <= cutoff)
+                # Check 2nd sudden losses criterion
+                crit2[row_i, col_j - 2] <- (data_loop[row_i, col_j - 1] - data_loop[row_i, col_j] <= sg_crit2_pct * data_loop[row_i, col_j - 1])
 
-      # Calculate 2nd sudden gains criterion
-      crit2[i, j - 2] <- ((df[i, j - 1] - df[i, j]) <= (-1 * (.25 * df[i, j - 1])))
+                # Check 3rd sudden losses criterion
 
-      # Calculate 3rd sudden gains criterion
-      # Create pre and post indices for 3rd criterion
-      pre_indices <-
-        base::max(1, j - 3):(j - 1) # Create index for pregain
-      post_indices <-
-        j:min(base::ncol(df), j + 2) # Create index for postgain
+                # First, create pre and post indices for 3rd criterion
+                pre_indices <- base::max(1, col_j - 3):(col_j - 1) # Create index for pregain
+                post_indices <- col_j:min(base::ncol(data_loop), col_j + 2) # Create index for postgain
 
-      # print(base::paste("iPRE", pre_indices)) # Check pre indices
-      # print(base::paste("iPOST", post_indices)) # Check post indices
+                # Define pre and post mean, sdn and number of available data points for 3rd criterion
+                mean_pre <- base::mean(base::as.matrix(data_loop[row_i, base::c(pre_indices)]), na.rm = T)
+                mean_post <- base::mean(base::as.matrix(data_loop[row_i, base::c(post_indices)]), na.rm = T)
+                sd_pre <- stats::sd(base::as.matrix(data_loop[row_i, base::c(pre_indices)]), na.rm = T)
+                sd_post <- stats::sd(base::as.matrix(data_loop[row_i, base::c(post_indices)]), na.rm = T)
+                sum_n_pre <- base::sum(!is.na(data_loop[row_i, base::c(pre_indices)]), na.rm = T)
+                sum_n_post <- base::sum(!is.na(data_loop[row_i, base::c(post_indices)]), na.rm = T)
 
-      # Define pre and post mean and sd for 3rd criterion
-      mean_pre <- base::mean(base::as.matrix(df[i, base::c(pre_indices)]), na.rm = T)
-      mean_post <- base::mean(base::as.matrix(df[i, base::c(post_indices)]), na.rm = T)
-      sd_pre <- stats::sd(base::as.matrix(df[i, base::c(pre_indices)]), na.rm = T)
-      sd_post <- stats::sd(base::as.matrix(df[i, base::c(post_indices)]), na.rm = T)
-      sum_n_pre <- base::sum(!is.na(df[i, base::c(pre_indices)]), na.rm = T)
-      sum_n_post <- base::sum(!is.na(df[i, base::c(post_indices)]), na.rm = T)
+                # Check 3rd criterion for no missing at pre or post
 
-      # Calculate 3rd criterion for no missing at pre or post
-      if (sum_n_pre == 3 & sum_n_post == 3) {
-        crit3[i, j - 2] <-
-          mean_pre - mean_post < -1 * (2.776 * base::sqrt(((2 * sd_pre ^ 2) + (2 * sd_post ^ 2)) / (3 + 3 - 2)))
-      } else if (sum_n_pre == 2 &
-                 sum_n_post == 3) {
-        # Adjusting critical value for missing value in pregain mean
-        crit3[i, j - 2] <-
-          mean_pre - mean_post < -1 * (3.182 * base::sqrt(((2 * sd_pre ^ 2) + (2 * sd_post ^ 2)) / (2 + 3 - 2)))
-      } else if (sum_n_pre == 3 &
-                 sum_n_post == 2) {
-        # Adjusting critical value for one missing value in postgain mean
-        crit3[i, j - 2] <-
-          mean_pre - mean_post < -1 * (3.182 * base::sqrt(((2 * sd_pre ^ 2) + (2 * sd_post ^ 2))  / (3 + 2 - 2)))
-      } else if (sum_n_pre == 2 &
-                 sum_n_post == 2) {
-        # Adjusting critical value for one missing value in pregain and postgain mean each
-        crit3[i, j - 2] <-
-          mean_pre - mean_post < -1 * (4.303 * base::sqrt(((2 * sd_pre ^ 2) + (2 * sd_post ^ 2))  / (2 + 2 - 2)))
-      } else if (sum_n_pre < 2 | sum_n_post < 2) {
-        crit3[i, j - 2] <- NA
-      }
+                if (sum_n_pre == 3 & sum_n_post == 3) {
+                    crit3[row_i, col_j - 2] <- mean_post - mean_pre > 2.776 * base::sqrt(((2 * sd_pre ^ 2) + (2 * sd_post ^ 2)) / (3 + 3 - 2))
+
+                    # Adjust critical value for 1 missing value in pregain mean
+                } else if (sum_n_pre == 2 & sum_n_post == 3) {
+                    crit3[row_i, col_j - 2] <- mean_post - mean_pre > 3.182 * base::sqrt(((2 * sd_pre ^ 2) + (2 * sd_post ^ 2)) / (2 + 3 - 2))
+
+                    # Adjust critical value for 1 missing value in postgain mean
+                } else if (sum_n_pre == 3 & sum_n_post == 2) {
+                    crit3[row_i, col_j - 2] <- mean_post - mean_pre > 3.182 * base::sqrt(((2 * sd_pre ^ 2) + (2 * sd_post ^ 2))  / (3 + 2 - 2))
+
+                    # Adjust critical value for 1 missing value pregain and 1 missing value postgain
+                } else if (sum_n_pre == 2 & sum_n_post == 2) {
+                    crit3[row_i, col_j - 2] <- mean_post - mean_pre > 4.303 * base::sqrt(((2 * sd_pre ^ 2) + (2 * sd_post ^ 2))  / (2 + 2 - 2))
+                } else if (sum_n_pre < 2 | sum_n_post < 2) {
+                    crit3[row_i, col_j - 2] <- NA
+                }
+            }
+        }
+
+        # Multiply dataframes with information on whether sudden losses criteria 1, 2, and 3 are met
+        # 1 = criterion is met, 0 = criterion is not met, NA = not enough data to identify sudden losses
+        crit123 <- crit1 * crit2 * crit3
+
+        # Create empty list for renaming variables
+        sg_col_names <- base::c()
+        sg_col_names_crit1 <- base::c()
+        sg_col_names_crit2 <- base::c()
+        sg_col_names_crit3 <- base::c()
+
+        # Create new variable names for sudden losses variables
+        # If identify_sg_1to2 is TRUE, sg variables will start with "sg_1to2"
+        if (identify_sg_1to2 == FALSE) {
+            for (i in 1:(base::ncol(data_loop) - 3)) {
+                sg_col_names[i] <- base::paste0("sl_", i + 1, "to", i + 2)
+
+                sg_col_names_crit1[i] <- base::paste0("sl_crit1_", i + 1, "to", i + 2)
+                sg_col_names_crit2[i] <- base::paste0("sl_crit2_", i + 1, "to", i + 2)
+                sg_col_names_crit3[i] <- base::paste0("sl_crit3_", i + 1, "to", i + 2)
+            }
+
+            # If identify_sg_1to2 is FALSE, sg variables will start with "sg_2to3"
+        } else if (identify_sg_1to2 == TRUE) {
+            for (i in 1:(base::ncol(data_loop) - 3)) {
+                sg_col_names[i] <- base::paste0("sl_", i, "to", i + 1)
+
+                sg_col_names_crit1[i] <- base::paste0("sl_crit1_", i, "to", i + 1)
+                sg_col_names_crit2[i] <- base::paste0("sl_crit2_", i, "to", i + 1)
+                sg_col_names_crit3[i] <- base::paste0("sl_crit3_", i, "to", i + 1)
+            }
+
+        }
+
+        # Name sudden losses variables of main datafile
+        names(crit123) <- sg_col_names
+
+        # Name sudden losses variables individual datafiles with criteria 1, 2, 3
+        names(crit1) <- sg_col_names_crit1
+        names(crit2) <- sg_col_names_crit2
+        names(crit3) <- sg_col_names_crit3
+
+        # Export dataframe with information whether individual criteria were met
+        if (crit123_details == TRUE) {
+            data_crit123_details <- base::cbind(data_select[, 1], crit1, crit2, crit3)
+
+            # The name for the exported dataframe can be changed in the function
+            assign(name_crit123_details, data_crit123_details, envir = .GlobalEnv)
+        }
+
+        # Combine ID with results from identify sudden losses loop
+        data_crit123 <- base::cbind(data_select[, 1], crit123)
+
+        # Combine data with variables used to identify sudden losses
+        data_select %>%
+            dplyr::left_join(data_crit123, by = id_var_name) %>%
+            dplyr::arrange(!! rlang::sym(id_var_name)) %>%
+            tibble::as.tibble()
     }
-  }
 
-  # Multiply dataframes with information on whether sudden gains criteria are met
-  # 1 = Criterion is met, 0 = criterion is not met,  NA = not enough data to be able to perform calculation
-  crit123 <- crit1 * crit2 * crit3
-
-  # Change the name of the dataframe
-
-  # Check whether session 0 ie intake is in dataset and therefore sg counting needs to start at 1
-  if (sum(str_detect(names(data), "[:alpha:]0")) == 1) {
-
-    sl_col_names <- base::c()
-    for (i in 1:(base::ncol(df)-3)) {
-      sl_col_names[i] <- base::paste("sl_", i, "to", i + 1, sep = "")
-    }
-  }
-
-  # Check whether session 0 ie intake is in dataset and therefore sg counting needs to start at 1
-  if (sum(str_detect(names(data), "[:alpha:]0")) == 0) {
-
-    sl_col_names <- base::c()
-    for (i in 1:(base::ncol(df)-3)) {
-      sl_col_names[i] <- base::paste("sl_", i + 1, "to", i + 2, sep = "")
-    }
-  }
-
-
-  names(crit123) <- sl_col_names
-
-  # Bind information about each sudden gain criterion from loop with id number
-  # The following three dataframe are only needed to check the individual criteria for each id
-  data_crit1 <- base::cbind(data[ , 1], crit1)
-  data_crit2 <- base::cbind(data[ , 1], crit2)
-  data_crit3 <- base::cbind(data[ , 1], crit3)
-
-  # Bind information about sudden gain from loop with id number
-  data_crit123 <- base::cbind(data[ , 1], crit123)
-
-  data_col_number <- base::max(base::ncol(data))
-
-
-
-  if (identify_sl_1to2 == TRUE) {
-    data %>%
-      dplyr::left_join(data_crit123, by = id_var_name) %>%
-      as.tibble()
-  } else if (identify_sl_1to2 == FALSE) {
-    if (sum(str_detect(names(data), "[:alpha:]0")) == 1) {
-      data %>%
-        dplyr::left_join(data_crit123, by = id_var_name) %>%
-        select(-sl_1to2) %>%
-        as.tibble()
-    } else if (sum(str_detect(names(data), "[:alpha:]0")) == 0) {
-      data %>%
-        dplyr::left_join(data_crit123, by = id_var_name) %>%
-        as.tibble()
-    }
-
-  }
-}

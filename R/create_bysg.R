496aee850a1fa2178aff37d2df46db5fddcf3a45
Milan Wiedemann <milan.wiedemann@gmail.com>
2019-01-17 17:02:43 +0000

    add functionality to create by gain and by loss datasets

--- R/create_bysg.R
+++ R/create_bysg.R
@@ -1,60 +1,78 @@
 #' Create a dataset with one gain per row, referred to as \code{bysg} dataset
 #'
 #' @param data A dataset in wide format with an id variable and the sudden gains variables.
-#' @param cutoff A number specifying the cut-off for criterion 1.
+#' @param sg_crit1_cutoff A number specifying the cut-off for criterion 1.
 #' @param id_var_name A string of the id variable name.
 #' @param sg_var_name A string of the sudden gains variable name.
 
 #' @return A dataset with one row per sudden gain.
 #' @export
 
-create_bysg <- function(data, cutoff, id_var_name, sg_var_list, tx_start_var_name, tx_end_var_name, sg_var_name, sg_crit2_pct = .25, identify_sg_1to2 = FALSE, include_s0_extract = FALSE) {
+create_bysg <- function(data, sg_crit1_cutoff, id_var_name, sg_var_list, tx_start_var_name, tx_end_var_name, sg_var_name, sg_crit2_pct = .25, identify = "sg", identify_sg_1to2 = FALSE) {
 
   # Before doing anything, save the raw data that was put in function as data argument
   data_in <- data
 
   # Run identify_sg function first to find positions of gain
-  data_crit123 <- suddengains::identify_sg(data = data,
-                                           id_var_name = id_var_name,
-                                           cutoff = cutoff,
-                                           sg_var_list = sg_var_list,
-                                           sg_crit2_pct = sg_crit2_pct,
-                                           identify_sg_1to2 = identify_sg_1to2)
+
+  if (identify == "sg") {
+      data_crit123 <- suddengains::identify_sg(data = data,
+                                               id_var_name = id_var_name,
+                                               sg_crit1_cutoff = sg_crit1_cutoff,
+                                               sg_var_list = sg_var_list,
+                                               sg_crit2_pct = sg_crit2_pct,
+                                               identify_sg_1to2 = identify_sg_1to2)
+  } else if (identify == "sl") {
+      data_crit123 <- suddengains::identify_sl(data = data,
+                                               id_var_name = id_var_name,
+                                               sg_crit1_cutoff = sg_crit1_cutoff,
+                                               sg_var_list = sg_var_list,
+                                               sg_crit2_pct = sg_crit2_pct,
+                                               identify_sg_1to2 = identify_sg_1to2)
+  }
+
 
   # Set missings to zero to calculate in next step
   data_crit123[base::is.na(data_crit123)] <- 0
 
 
   data_bysg <- data_crit123 %>%
-    select(!! rlang::sym(id_var_name), matches("sg_\\d+to\\d+")) %>%
-    gather(key = "session", value = "sg_crit123", -!! rlang::sym(id_var_name)) %>%
-    mutate(session = as.numeric(str_extract(session, "(\\d+)")),
+    dplyr::select(!! rlang::sym(id_var_name), dplyr::matches("sg_|sl_\\d+to\\d+")) %>%
+    tidyr::gather(key = "session", value = "sg_crit123", -!! rlang::sym(id_var_name)) %>%
+    dplyr::mutate(session = as.numeric(stringr::str_extract(session, "(\\d+)")),
            sg_session_n = session) %>%
-    arrange(!! rlang::sym(id_var_name)) %>%
-    group_by(!! rlang::sym(id_var_name)) %>%
-    mutate(sg_freq_byperson = sum(sg_crit123, na.rm = TRUE)) %>%
-    arrange(desc(sg_freq_byperson)) %>%
-    filter(sg_crit123 != 0) %>%
-    select(-session) %>%
-    mutate(id_sg = paste0(!! rlang::sym(id_var_name), "_sg_", sg_session_n)) %>%
-    select(!! rlang::sym(id_var_name), id_sg, everything()) %>%
-    ungroup()
+    dplyr::arrange(!! rlang::sym(id_var_name)) %>%
+    dplyr::group_by(!! rlang::sym(id_var_name)) %>%
+    dplyr::mutate(sg_freq_byperson = sum(sg_crit123, na.rm = TRUE)) %>%
+    dplyr::arrange(desc(sg_freq_byperson)) %>%
+    dplyr::filter(sg_crit123 != 0) %>%
+    dplyr::select(-session) %>%
+    dplyr::mutate(id_sg = paste0(!! rlang::sym(id_var_name), "_sg_", sg_session_n)) %>%
+    dplyr::select(!! rlang::sym(id_var_name), id_sg, everything()) %>%
+    dplyr::ungroup()
 
   data_bysg <- data_bysg %>%
-    left_join(select(data_in, id_var_name, sg_var_list), by = id_var_name)
+      dplyr::left_join(dplyr::select(data_in, id_var_name, sg_var_list), by = id_var_name)
+
+  # Set start value for numbering to extract the correct values around the gain
+  if (identify_sg_1to2 == TRUE) {
+      start_numbering <- 0
+  } else if (identify_sg_1to2 == FALSE) {
+      start_numbering <- 1
+  }
 
   # Extract scores of the sudden gains measure around the sudden gain ----
   data_extract <- suddengains::extract_values(data_bysg,
                                               id_var_name = "id_sg",
                                               extract_var_list = sg_var_list,
                                               extract_var_name = sg_var_name,
-                                              include_s0_extract = include_s0_extract,
-                                              combine_datasets = FALSE
+                                              start_numbering = start_numbering,
+                                              add_to_data = FALSE
                                               )
 
   # Combine the extracted scores with the bysg dataset
   data_bysg <- data_bysg %>%
-    left_join(data_extract, by = "id_sg")
+    dplyr::left_join(data_extract, by = "id_sg")
 
   # Calculate descriptive sudden gains variables ----
 
@@ -80,30 +98,26 @@ create_bysg <- function(data, cutoff, id_var_name, sg_var_list, tx_start_var_nam
   id_sg_list <- data_bysg$id_sg
 
   sg_reversal <- data_bysg %>%
-    select(id_sg, sg_session_n, sg_reversal_value, sg_var_list) %>%
-    rename_sg_vars(sg_var_list, start_numbering = 0) %>%
+    dplyr::select(id_sg, sg_session_n, sg_reversal_value, sg_var_list) %>%
+    suddengains::rename_sg_vars(sg_var_list, start_numbering = 0) %>%
     tidyr::gather(key = "time_str", value = "value", -id_sg, -sg_session_n, -sg_reversal_value) %>%
     dplyr::mutate(time_num = as.numeric(str_extract(time_str, "\\d+"))) %>%
-    select(-time_str) %>%
+    dplyr::select(-time_str) %>%
     dplyr::mutate(index = time_num - sg_session_n) %>%
     dplyr::filter(index >= 2) %>%
-    arrange(id_sg, time_num) %>%
-    group_by(id_sg) %>%
-    mutate(sg_reversal = if_else(value > sg_reversal_value, 1, 0),
-           sg_reversal = if_else(sg_reversal == 1, 1, 0)) %>%
+    dplyr::arrange(id_sg, time_num) %>%
+    dplyr::group_by(id_sg) %>%
+    dplyr::mutate(sg_reversal = dplyr::if_else(value > sg_reversal_value, 1, 0),
+           sg_reversal = dplyr::if_else(sg_reversal == 1, 1, 0)) %>%
     dplyr::select(id_sg, sg_reversal) %>%
-    filter(sg_reversal == 1) %>%
-    unique() %>%
-    ungroup() %>%
+    dplyr::filter(sg_reversal == 1) %>%
+    base::unique() %>%
+    dplyr::ungroup() %>%
     tidyr::complete(id_sg = id_sg_list) %>%
     tidyr::replace_na(list(sg_reversal = 0))
 
   data_bysg <- data_bysg %>%
-    left_join(sg_reversal, by = "id_sg")
-
-
-
-
+    dplyr::left_join(sg_reversal, by = "id_sg")
 
   # Return tibble
 

3dd74a043c705e104db59000579cbc70155970fc
Milan Wiedemann <milan.wiedemann@gmail.com>
2019-01-29 14:27:06 +0000

    first structure to merge the plot functions

--- R/plot_sg.R
+++ R/plot_sg.R
@@ -1,5 +1,9 @@
 #' Plot average magnitude of sudden gain or another variable around the sudden gain
 #'
+#' @description Generates a plot of the mean values around the sudden gain.
+#' This can be used to plot the primary outcome or secondary measures.
+#' The parameters starting with "group" allow to plot the average gain magnitude by group.
+#'
 #' @param data A \code{bysg} or \code{byperson} dataset.
 #' @param tx_start_var_name String, specifying the variable name of the first measurement point of the intervention.
 #' @param tx_end_var_name String, specifying the variable name of the last measurement point of the intervention.
@@ -23,213 +27,123 @@ plot_sg <- function(data, tx_start_var_name, tx_end_var_name, sg_pre_post_var_li
         post2 <- sg_pre_post_var_list[5]
         post3 <- sg_pre_post_var_list[6]
 
+        # Check if group vars are specified
+        if (is.null(group_var_name) && is.null(group_levels) && is.null(group_labels)) {
 
-        # Do some sort of checking here whether group variables are specified
-        # If not
+            group <- 0
 
-        group_sg_plot <- 1
+            group_var_name <- 1
 
-        # Prepare data for plotting without group
-        plot_data <- data %>%
-            dplyr::select(start_fun = tx_start_var_name,
-                          pre3_fun = pre3,
-                          pre2_fun = pre2,
-                          pre1_fun = pre1,
-                          post1_fun = post1,
-                          post2_fun = post2,
-                          post3_fun = post3,
-                          end_fun = tx_end_var_name) %>%
-            tidyr::gather(variable, value) %>%
-            dplyr::mutate(variable = factor(variable,
-                                     levels = c("start_fun", "pre3_fun", "pre2_fun", "pre1_fun",
-                                                "post1_fun", "post2_fun", "post3_fun", "end_fun")))
+            # Prepare data for plotting without group
+            plot_data <- data %>%
+                dplyr::select(start_fun = tx_start_var_name,
+                              pre3_fun = pre3,
+                              pre2_fun = pre2,
+                              pre1_fun = pre1,
+                              post1_fun = post1,
+                              post2_fun = post2,
+                              post3_fun = post3,
+                              end_fun = tx_end_var_name) %>%
+                tidyr::gather(variable, value) %>%
+                dplyr::mutate(variable = factor(variable,
+                                                levels = c("start_fun", "pre3_fun", "pre2_fun", "pre1_fun",
+                                                           "post1_fun", "post2_fun", "post3_fun", "end_fun")))
 
-        # # Prepare data for plotting groups
-        # plot_data <- data %>%
-        #     dplyr::select(start_fun = tx_start_var_name,
-        #                   pre3_fun = pre3,
-        #                   pre2_fun = pre2,
-        #                   pre1_fun = pre1,
-        #                   post1_fun = post1,
-        #                   post2_fun = post2,
-        #                   post3_fun = post3,
-        #                   end_fun = tx_end_var_name,
-        #                   group_var_name_fun) %>%
-        #     tidyr::gather(variable, value, -group_var_name) %>%
-        #     dplyr::mutate(variable = factor(variable,
-        #                                     levels = c("start_fun", "pre3_fun", "pre2_fun", "pre1_fun",
-        #                                                "post1_fun", "post2_fun", "post3_fun", "end_fun"))) %>%
-        #     dplyr::mutate(group_var_name = factor(!!rlang::sym(group_var_name)))
-        #
-        #
-        # # Add levels and labels to group factor
-        # plot_sg_group$group_var_name <- factor(plot_sg_group$group_var_name,
-        #                                        levels = group_levels,
-        #                                        labels = group_labels)
+            sg_plot_start <- ggplot2::ggplot(data = plot_data,
+                                             aes(x = variable,
+                                                 y = value))
 
-        # Create plot
-        ggplot2::ggplot(data = plot_data,
-                        aes(x = variable,
-                            y = value)) +
+        } else if (!is.null(group_var_name) && !is.null(group_levels) && !is.null(group_labels)) {
 
-            # Add means
-            ggplot2::stat_summary(fun.y = mean,
-                                  geom = "point",
-                                  colour = "#00BFC4",
-                                  position = position_dodge(width = 0.2)) +
+            group <- 1
 
-            # Add 95pct CIs
-            ggplot2::stat_summary(fun.data = mean_cl_normal,
-                                  geom = "errorbar",
-                                  fun.args = list(mult = 1.96),
-                                  width = 0.1,
-                                  colour = "#00BFC4",
-                                  position = position_dodge(width = 0.2)) +
-
-            # Add dotted line from start value to pre-pre-pre gain value
-            ggplot2::stat_summary(data = dplyr::filter(plot_data, variable %in% c("start_fun", "pre3_fun")),
-                                  aes(y = value, group = group_sg_plot),
-                                  fun.y = mean,
-                                  geom = "line",
-                                  linetype = 3,
-                                  colour = "#00BFC4",
-                                  position = position_dodge(width = 0.2)) +
-
-            ggplot2::stat_summary(data = dplyr::filter(plot_data, variable %in% c("pre3_fun", "pre2_fun", "pre1_fun",
-                                                                                  "post1_fun", "post2_fun", "post3_fun")),
-                                  aes(y = value, group = group_sg_plot),
-                                  fun.y = mean,
-                                  geom = "line",
-                                  linetype = 1,
-                                  colour = "#00BFC4",
-                                  position = position_dodge(width = 0.2)) +
-
-            # Add dotted line from post-post-post gain value to end value
-            ggplot2::stat_summary(data = dplyr::filter(plot_data, variable %in% c("post3_fun", "end_fun")),
-                                  aes(y = value, group = group_sg_plot),
-                                  fun.y = mean,
-                                  geom = "line",
-                                  linetype = 3,
-                                  colour = "#00BFC4",
-                                  position = position_dodge(width = 0.2)) +
-
-            # Add scale for x axis
-            ggplot2::scale_x_discrete(labels = c("Start", "N-2", "N-1", "N",
-                                                 "N+1", "N+2", "N+3", "End")) +
-
-            # Add x and y axis labels
-            ggplot2::xlab(xlab) +
-            ggplot2::ylab(ylab) +
-
-            # Make plot look APAish
-            ggplot2::theme_classic() +
-            ggplot2::theme(text = element_text(size = 12))
-    }
-
-#' Plot average magnitude of sudden gain or another variable around the sudden gain
-#'
-#' @param data A matched \code{byperson} dataset with variable sg_crit123 (0,1) as group variable.
-#' @param var_group Name of group variable.
-#' @param group_labels A c() of string as labels for groups ordered consistent with numbering of group in variable.
-#' @param ylab A string as label for y-Axis.
-#' @param start A string of the variable name with scores at the start of treatment.
-#' @param pre3 A string of the pre3 variable name.
-#' @param pre2 A string of the pre2 variable name.
-#' @param pre1 A string of the pre1 variable name.
-#' @param post1 A string of the post1 variable name.
-#' @param post2 A string of the post2 variable name.
-#' @param post3 A string of the post3 variable name.
-#' @param end A string of the variable name with scores at the end of treatment.
-#' @return A plot of the average gain around or changes in scores of another questionnaire around the sudden gain.
-#' @export
-
-plot_sg_group <-
-    function(data,
-             var_group,
-             group_levels,
-             group_labels,
-             ylab,
-             start,
-             pre3,
-             pre2,
-             pre1,
-             post1,
-             post2,
-             post3,
-             end) {
-        plot_sg_group <- data %>%
-            dplyr::select(var_group,
-                          start, pre3, pre2, pre1, post1, post2, post3, end) %>%
-            tidyr::gather(variable, value,-var_group) %>%
-            dplyr::mutate(variable = factor(
-                variable,
-                levels = c(start, pre3, pre2, pre1, post1, post2, post3, end)
-            )) %>%
-            dplyr::mutate(var_group = factor(!!rlang::sym(var_group)))
+            # Prepare data for plotting groups
+            plot_data <- data %>%
+                dplyr::select(start_fun = tx_start_var_name,
+                              pre3_fun = pre3,
+                              pre2_fun = pre2,
+                              pre1_fun = pre1,
+                              post1_fun = post1,
+                              post2_fun = post2,
+                              post3_fun = post3,
+                              end_fun = tx_end_var_name,
+                              group_var_name) %>%
+                tidyr::gather(variable, value, -group_var_name) %>%
+                dplyr::mutate(variable = factor(variable,
+                                                levels = c("start_fun", "pre3_fun", "pre2_fun", "pre1_fun",
+                                                           "post1_fun", "post2_fun", "post3_fun", "end_fun"))) %>%
+                dplyr::mutate(group_var_name = factor(!!rlang::sym(group_var_name)))
 
 
-        # Add names to factors
-        plot_sg_group$var_group <-
-            factor(plot_sg_group$var_group,
-                   levels = group_levels,
-                   labels = group_labels)
+            # Add levels and labels to group factor
+            plot_data$group_var_name <- factor(plot_data$group_var_name,
+                                               levels = group_levels,
+                                               labels = group_labels)
 
-        ggplot2::ggplot(plot_sg_group,
-                        aes(
-                            x = variable,
-                            y = value,
-                            # Add colour and shape for groups
-                            colour = var_group,
-                            shape = var_group
-                        )) +
-            stat_summary(
-                data = plot_sg_group,
-                fun.y = mean,
-                geom = "point",
-                position = position_dodge(width = 0.2)
-            ) +
-            stat_summary(
-                data = plot_sg_group,
-                fun.data = mean_cl_normal,
-                geom = "errorbar",
-                width = 0.2,
-                fun.args = list(mult = 1.96),
-                position = position_dodge(width = 0.2)
-            ) +
-            stat_summary(
-                data = filter(plot_sg_group, variable %in% c(start, pre3)),
-                aes(y = value, group = var_group),
-                fun.y = mean,
-                geom = "line",
-                linetype = 3,
-                position = position_dodge(width = 0.2)
-            ) +
-            stat_summary(
-                data = filter(
-                    plot_sg_group,
-                    variable %in% c(pre3, pre2, pre1, post1, post2, post3)
-                ),
-                aes(y = value, group = var_group),
-                fun.y = mean,
-                geom = "line",
-                linetype = 1,
-                position = position_dodge(width = 0.2)
-            ) +
-            stat_summary(
-                data = filter(plot_sg_group, variable %in% c(post3, end)),
-                aes(y = value, group = var_group),
-                fun.y = mean,
-                geom = "line",
-                linetype = 3,
-                position = position_dodge(width = 0.2)
-            ) +
-            theme_classic() +
-            theme(text = element_text(size = 12)) +
-            scale_x_discrete(labels = c("Start", "N-2", "N-1", "N", "N+1", "N+2", "N+3", "End")) +
-            labs(
-                x = "Session",
-                y = ylab,
-                colour = "",
-                shape = ""
-            )
+            sg_plot_start <- ggplot2::ggplot(data = plot_data,
+                                             aes(x = variable,
+                                                 y = value,
+                                                 colour = group_var_name,
+                                                 shape = group_var_name))
+        }
+
+        # Create plot ----
+
+        sg_plot_start +
+
+        # Add means
+        ggplot2::stat_summary(fun.y = mean,
+                              geom = "point",
+                              # colour = "#00BFC4",
+                              position = position_dodge(width = 0.2)) +
+
+        # Add 95pct CIs
+        ggplot2::stat_summary(fun.data = mean_cl_normal,
+                              geom = "errorbar",
+                              fun.args = list(mult = 1.96),
+                              width = 0.1,
+                              # colour = "#00BFC4",
+                              position = position_dodge(width = 0.2)) +
+
+        # Add dotted line from start value to pre-pre-pre gain value
+        ggplot2::stat_summary(data = dplyr::filter(plot_data, variable %in% c("start_fun", "pre3_fun")),
+                              aes(y = value, group = group_var_name),
+                              fun.y = mean,
+                              geom = "line",
+                              linetype = 3,
+                              # colour = "#00BFC4",
+                              position = position_dodge(width = 0.2)) +
+
+        ggplot2::stat_summary(data = dplyr::filter(plot_data, variable %in% c("pre3_fun", "pre2_fun", "pre1_fun",
+                                                                              "post1_fun", "post2_fun", "post3_fun")),
+                              aes(y = value, group = group_var_name),
+                              fun.y = mean,
+                              geom = "line",
+                              linetype = 1,
+                              # colour = "#00BFC4",
+                              position = position_dodge(width = 0.2)) +
+
+        # Add dotted line from post-post-post gain value to end value
+        ggplot2::stat_summary(data = dplyr::filter(plot_data, variable %in% c("post3_fun", "end_fun")),
+                              aes(y = value, group = group_var_name),
+                              fun.y = mean,
+                              geom = "line",
+                              linetype = 3,
+                              # colour = "#00BFC4",
+                              position = position_dodge(width = 0.2)) +
+
+        # Add scale for x axis
+        ggplot2::scale_x_discrete(labels = c("Start", "N-2", "N-1", "N",
+                                             "N+1", "N+2", "N+3", "End")) +
+
+        # Add x and y axis labels
+        ggplot2::labs(x = xlab,
+                      y = ylab,
+                      colour = "",
+                      shape = ""
+                      ) +
+
+        # Make plot look APAish
+        ggplot2::theme_classic() +
+        ggplot2::theme(text = element_text(size = 12))
     }

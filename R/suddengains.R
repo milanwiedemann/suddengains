27e96baad90db358cb2874ccbe394192d350fc1f
Milan Wiedemann <milan.wiedemann@gmail.com>
2019-04-03 16:43:19 +0100

    set all the globalVariables here, CMD check didnt like it without, now all fine

---
+++ R/suddengains.R
@@ -0,0 +1,2 @@
+utils::globalVariables(c("id_sg", "sg_session_n", "sg_magnitude", "session",  "sg_magnitude", "session", "sg_crit123", "sg_freq_byperson", "sg_magnitude", "sg_reversal_value", "time_str", "time_num", "index", "value", "sg_crit123", ".", "sg_freq_byperson", "sg_reversal", "sg_magnitude", "time_str", "time_num", "index", "name", "value", "x_n_pre2", "x_n_pre1", "x_n", "x_n_post1", "x_n_post2", "x_n_post3", "variable", "value", "pattern", "sg_pattern_1", "sg_pattern_2", "sg_pattern_3", "sg_pattern_4", "sg_select", "nvalid"))
+

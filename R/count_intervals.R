65593d2a452346c439187a3d9b291f96efa18467
Milan Wiedemann <milan.wiedemann@gmail.com>
2019-04-21 12:39:38 +0100

    supress message from identify sg, it's only confusing here and not relevant!
    change object names for analysed and not analysed in the returned list to make it easier to understand what these numbers are

--- R/count_intervals.R
+++ R/count_intervals.R
@@ -11,15 +11,15 @@
 #'   \describe{
 #'     \item{total_between_sess_intervals}{: The total number of between-session intervals present in the data set}
 #'     \item{total_between_sess_intervals_sg}{: The total number of gain intervals (i.e. sudden gains) present in the data set}
-#'     \item{sg_sess_sum_analysed}{: The total number of between-session intervals that could be analysed for sudden gains}
-#'     \item{sg_sess_sum_not_analysed}{: The total number of between-session intervals that could not be analysed for sudden gains (due to missing data)}
+#'     \item{analysed_between_sess_intervals_sg}{: The total number of between-session intervals that could be analysed for sudden gains}
+#'     \item{not_analysed_between_sess_intervals_sg}{: The total number of between-session intervals that could not be analysed for sudden gains (due to missing data)}
 #'     }
 #' @export
 
 count_intervals <- function(data, id_var_name, sg_var_list, identify_sg_1to2 = FALSE) {
 
     # First, run identify_sg() function to identify sudden gains and store results
-    data_sg <- suddengains::identify_sg(data = data,
+    data_sg <- suppressMessages(suddengains::identify_sg(data = data,
                                         # Set this to 1 as it doesnt matter for counting, between session intervals
                                         # I just need to know where all three criteria can be applied
                                         sg_crit1_cutoff = 1,
@@ -28,7 +28,7 @@ count_intervals <- function(data, id_var_name, sg_var_list, identify_sg_1to2 = F
                                         id_var_name = id_var_name,
                                         sg_var_list = sg_var_list,
                                         identify_sg_1to2 = identify_sg_1to2
-                                        )
+                                        ))
 
     total_between_sess_intervals <- (base::length(sg_var_list) - 1) * base::nrow(data)
 
@@ -42,15 +42,15 @@ count_intervals <- function(data, id_var_name, sg_var_list, identify_sg_1to2 = F
     # This function only counts intervals where it was possible to identify sudden gains:
     # If not all three criteria can be applied to an interval because of too many missing scores before or after the interval this function returns NA
     # Therefore this functions is not a simple count of between session intervals in the data but is more closely linked to sudden gains analysis
-    sg_sess_sum_analysed <- base::sum(!is.na(dplyr::select(data_sg, dplyr::starts_with("sg_"))))
+    analysed_between_sess_intervals_sg <- base::sum(!is.na(dplyr::select(data_sg, dplyr::starts_with("sg_"))))
 
     # Count number of between session intervals where the three sudden gains criteria could not be applied
-    sg_sess_sum_not_analysed <- base::sum(is.na(dplyr::select(data_sg, dplyr::starts_with("sg_"))))
+    not_analysed_between_sess_intervals_sg <- base::sum(is.na(dplyr::select(data_sg, dplyr::starts_with("sg_"))))
 
     sg_sess_list <- base::list(total_between_sess_intervals = total_between_sess_intervals,
                          total_between_sess_intervals_sg = total_between_sess_intervals_sg,
-                         sg_sess_sum_analysed = sg_sess_sum_analysed,
-                         sg_sess_sum_not_analysed = sg_sess_sum_not_analysed)
+                         analysed_between_sess_intervals_sg = analysed_between_sess_intervals_sg,
+                         not_analysed_between_sess_intervals_sg = not_analysed_between_sess_intervals_sg)
 
 
     # Return number of between session intervals that were analysed / not analysed for sudden gains

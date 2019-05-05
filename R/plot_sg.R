8c4198cb2c7de658bb2a7bf967a424fe736543a3
Milan Wiedemann <milan.wiedemann@gmail.com>
2019-05-05 12:18:26 +0100

    fix wrong starting values for byperson (no group), add apaish feature

--- R/plot_sg.R
+++ R/plot_sg.R
@@ -1,18 +1,16 @@
-#' Plot average magnitude of sudden gain or another variable around the sudden gain
+#' Plot average change in variables around the sudden gain
 #'
 #' @description Generates a plot of the mean values around the sudden gain.
 #' This can be used to plot the primary outcome or secondary measures.
 #' The parameters starting with "group" allow to plot the average gain magnitude by group.
-#'
 #' @param data A \code{bysg} or \code{byperson} dataset.
+#' @param id_var_name String, specifying the name of the ID variable.
 #' @param tx_start_var_name String, specifying the variable name of the first measurement point of the intervention.
 #' @param tx_end_var_name String, specifying the variable name of the last measurement point of the intervention.
 #' @param sg_pre_post_var_list Vector, specifying the variable names of the 3 measurement points before,
 #' and the 3 after the sudden gain, for the measure being plotted.
 #' @param colour Vector, specifying the string(s) of the colour(s) of the plot.
 #' In case groups were specified, the order of this vector will apply colours to the plot in the same order as \code{group_levels}.
-#' WARNING WARNING WARNING, AT THE MOMENT IT IS ONLY POSSIBLE TO SPECIFY ONE COLOUR FOR ALL GROUPS.
-#' The default at the moment is my favourite colour.
 #' @param group_var_name String, specifying the variable name of the group variable.
 #' @param group_levels Vector, specifying the levels as numeric for the groups in \code{group_var_name}.
 #' @param group_labels Vector, specifying the label names as strings for the groups in \code{group_var_name}.
@@ -41,82 +39,98 @@
 #'                                  "sg_bdi_n1", "sg_bdi_n2", "sg_bdi_n3"),
 #'         ylab = "BDI", xlab = "Session")
 
-plot_sg <- function(data, tx_start_var_name, tx_end_var_name, sg_pre_post_var_list, ylab, xlab = "Session", colour = "#239b89ff", group_var_name = NULL, group_levels = NULL, group_labels = NULL, group_title = NULL) {
+plot_sg <- function(data, id_var_name, tx_start_var_name, tx_end_var_name, sg_pre_post_var_list, ylab, xlab = "Session", colour = "#239b89ff", group_var_name = NULL, group_levels = NULL, group_labels = NULL, group_title = NULL, APAish = TRUE) {
 
-        # Save variable names to individual objects
-        pre3 <- sg_pre_post_var_list[1]
-        pre2 <- sg_pre_post_var_list[2]
-        pre1 <- sg_pre_post_var_list[3]
-        post1 <- sg_pre_post_var_list[4]
-        post2 <- sg_pre_post_var_list[5]
-        post3 <- sg_pre_post_var_list[6]
+    # Create data for plot ----
+    # Save variable names to individual objects
+    pre3 <- sg_pre_post_var_list[1]
+    pre2 <- sg_pre_post_var_list[2]
+    pre1 <- sg_pre_post_var_list[3]
+    post1 <- sg_pre_post_var_list[4]
+    post2 <- sg_pre_post_var_list[5]
+    post3 <- sg_pre_post_var_list[6]
 
-        # Check if group vars are specified
-        if (base::is.null(group_var_name) && base::is.null(group_levels) && base::is.null(group_labels)) {
+    # Check group vars ----
+    if (base::is.null(group_var_name) && base::is.null(group_levels) && base::is.null(group_labels)) {
 
-            group <- 0
+        # Set values for group variables
+        group <- 0
+        group_var_name <- 1
 
-            group_var_name <- 1
+        # Create start end tx data
+        # Only use unique values for start and end, otherwise these will be included multiple times in case of multiple gains if data is bysg
+        plot_data_start_end <- data %>%
+            select(id_var_name, id_sg, sg_crit123, start_fun = tx_start_var_name, end_fun = tx_end_var_name) %>%
+            filter(sg_crit123 == 1) %>%
+            dplyr::distinct(!! rlang::sym(id_var_name), .keep_all = TRUE) %>%
+            select(-sg_crit123)
 
-            # Prepare data for plotting without group
-            plot_data <- data %>%
-                dplyr::select(start_fun = tx_start_var_name,
-                              pre3_fun = pre3,
-                              pre2_fun = pre2,
-                              pre1_fun = pre1,
-                              post1_fun = post1,
-                              post2_fun = post2,
-                              post3_fun = post3,
-                              end_fun = tx_end_var_name) %>%
-                tidyr::gather(variable, value) %>%
-                dplyr::mutate(variable = base::factor(variable, levels = c("start_fun", "pre3_fun", "pre2_fun", "pre1_fun",
-                                                                           "post1_fun", "post2_fun", "post3_fun", "end_fun")))
+        # Create pre end post gain
+        # Use all available data here, all multiple gains are included here if data is bysg
+        plot_data_pre_post <- data %>%
+            dplyr::select(id_var_name, id_sg,
+                          pre3_fun = pre3, pre2_fun = pre2, pre1_fun = pre1,
+                          post1_fun = post1, post2_fun = post2, post3_fun = post3)
 
-            sg_plot_start <- ggplot2::ggplot(data = plot_data,
-                                             ggplot2::aes(x = variable,
-                                                          y = value))
+        # Join both data sets
+        plot_data_start_end_pre_post <- dplyr::left_join(plot_data_pre_post, plot_data_start_end, by = c("id_sg", id_var_name))
 
-        } else if (!base::is.null(group_var_name) && !base::is.null(group_levels) && !base::is.null(group_labels)) {
+        # Wide to long format and order factors for plot
+        plot_data <- plot_data_start_end_pre_post %>%
+            dplyr::select(start_fun,
+                          pre3_fun, pre2_fun, pre1_fun,
+                          post1_fun, post2_fun, post3_fun,
+                          end_fun) %>%
+            tidyr::gather(variable, value) %>%
+            dplyr::mutate(variable = base::factor(variable, levels = c("start_fun", "pre3_fun", "pre2_fun", "pre1_fun",
+                                                                       "post1_fun", "post2_fun", "post3_fun", "end_fun")))
 
-            group <- 1
+        # Start plot
+        sg_plot_start <- ggplot2::ggplot(data = plot_data,
+                                         ggplot2::aes(x = variable,
+                                                      y = value))
 
-            # Prepare data for plotting groups
-            plot_data <- data %>%
-                dplyr::select(start_fun = tx_start_var_name,
-                              pre3_fun = pre3,
-                              pre2_fun = pre2,
-                              pre1_fun = pre1,
-                              post1_fun = post1,
-                              post2_fun = post2,
-                              post3_fun = post3,
-                              end_fun = tx_end_var_name,
-                              group_var_name) %>%
-                tidyr::gather(variable, value, -group_var_name) %>%
-                dplyr::mutate(variable = base::factor(variable, levels = base::c("start_fun", "pre3_fun", "pre2_fun", "pre1_fun",
-                                                                                 "post1_fun", "post2_fun", "post3_fun", "end_fun"))) %>%
-                dplyr::mutate(group_var_name = base::factor(!! rlang::sym(group_var_name)))
+    } else if (!base::is.null(group_var_name) && !base::is.null(group_levels) && !base::is.null(group_labels)) {
+
+        group <- 1
+
+        # Prepare data for plotting groups ----
+        plot_data <- data %>%
+            dplyr::select(start_fun = tx_start_var_name,
+                          pre3_fun = pre3,
+                          pre2_fun = pre2,
+                          pre1_fun = pre1,
+                          post1_fun = post1,
+                          post2_fun = post2,
+                          post3_fun = post3,
+                          end_fun = tx_end_var_name,
+                          group_var_name) %>%
+            tidyr::gather(variable, value, -group_var_name) %>%
+            dplyr::mutate(variable = base::factor(variable, levels = base::c("start_fun", "pre3_fun", "pre2_fun", "pre1_fun",
+                                                                             "post1_fun", "post2_fun", "post3_fun", "end_fun"))) %>%
+            dplyr::mutate(group_var_name = base::factor(!! rlang::sym(group_var_name)))
 
 
-            # Add levels and labels to group factor
-            plot_data$group_var_name <- base::factor(plot_data$group_var_name,
-                                                     levels = group_levels,
-                                                     labels = group_labels)
+        # Add levels and labels to group factor
+        plot_data$group_var_name <- base::factor(plot_data$group_var_name,
+                                                 levels = group_levels,
+                                                 labels = group_labels)
 
-            sg_plot_start <- ggplot2::ggplot(data = plot_data,
-                                             ggplot2::aes(x = variable,
-                                                          y = value,
-                                                          colour = group_var_name,
-                                                          shape = group_var_name))
-        }
 
-        # Create plot ----
+        sg_plot_start <- ggplot2::ggplot(data = plot_data,
+                                         ggplot2::aes(x = variable,
+                                                      y = value,
+                                                      colour = group_var_name,
+                                                      shape = group_var_name))
+    }
 
-        sg_plot_start +
+    # Create plot ----
+    plot_out <- sg_plot_start +
 
         # Add means
         ggplot2::stat_summary(fun.y = mean,
                               geom = "point",
-                              colour = colour,
+                              # colour = colour,
                               position = ggplot2::position_dodge(width = 0.2)) +
 
         # Add 95pct CIs
@@ -124,7 +138,7 @@ plot_sg <- function(data, tx_start_var_name, tx_end_var_name, sg_pre_post_var_li
                               geom = "errorbar",
                               fun.args = base::list(mult = 1.96),
                               width = 0.1,
-                              colour = colour,
+                              # colour = colour,
                               position = ggplot2::position_dodge(width = 0.2)) +
 
         # Add dotted line from start value to pre-pre-pre gain value
@@ -133,7 +147,7 @@ plot_sg <- function(data, tx_start_var_name, tx_end_var_name, sg_pre_post_var_li
                               fun.y = mean,
                               geom = "line",
                               linetype = 3,
-                              colour = colour,
+                              # colour = colour,
                               position = ggplot2::position_dodge(width = 0.2)) +
 
         ggplot2::stat_summary(data = dplyr::filter(plot_data, variable %in% base::c("pre3_fun", "pre2_fun", "pre1_fun",
@@ -142,7 +156,7 @@ plot_sg <- function(data, tx_start_var_name, tx_end_var_name, sg_pre_post_var_li
                               fun.y = mean,
                               geom = "line",
                               linetype = 1,
-                              colour = colour,
+                              # colour = colour,
                               position = ggplot2::position_dodge(width = 0.2)) +
 
         # Add dotted line from post-post-post gain value to end value
@@ -151,7 +165,7 @@ plot_sg <- function(data, tx_start_var_name, tx_end_var_name, sg_pre_post_var_li
                               fun.y = mean,
                               geom = "line",
                               linetype = 3,
-                              colour = colour,
+                              # colour = colour,
                               position = ggplot2::position_dodge(width = 0.2)) +
 
         # Add scale for x axis
@@ -162,10 +176,16 @@ plot_sg <- function(data, tx_start_var_name, tx_end_var_name, sg_pre_post_var_li
         ggplot2::labs(x = xlab,
                       y = ylab,
                       colour = NULL,
-                      shape = group_title
-                      ) +
+                      shape = group_title)
 
+    if (APAish == TRUE) {
         # Make plot look APAish
-        ggplot2::theme_classic() +
-        ggplot2::theme(text = ggplot2::element_text(size = 12))
+        plot_out +
+            ggplot2::theme_classic() +
+            ggplot2::theme(text = ggplot2::element_text(size = 14))
+
+    } else {
+        plot_out
     }
+
+}

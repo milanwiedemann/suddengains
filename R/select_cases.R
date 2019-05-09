0635f5812743b426f361f7b857a742c6d0c60786
Milan Wiedemann <milan.wiedemann@gmail.com>
2019-05-09 10:58:06 +0100

    add table to select cases help illustrating pattern method

--- R/select_cases.R
+++ R/select_cases.R
@@ -1,6 +1,17 @@
 #' Select sample providing enough data to identify sudden gains
 #'
 #' @description Select sample for further sudden gains analyses depending on specified methods.
+#'
+#' The following table shows the different data patterns that get selected when \code{method = "pattern"}.
+#' This function goes through the data and selects all cases with at least one of the following data patterns.
+#' \tabular{lrrrrrr}{
+#' \strong{Pattern} \tab \strong{x1} \tab \strong{x2} \tab \strong{x3} \tab \strong{x4} \tab \strong{x5} \tab \strong{x6} \cr
+#' \strong{1.}       \tab x           \tab \strong{N}  \tab x           \tab x           \tab .         \tab .           \cr
+#' \strong{2.}       \tab x           \tab \strong{N}  \tab x           \tab .          \tab x          \tab .           \cr
+#' \strong{3.}       \tab x           \tab .          \tab \strong{N}  \tab x           \tab x          \tab .           \cr
+#' \strong{4.}       \tab x           \tab .          \tab \strong{N}  \tab x           \tab .         \tab x
+#' }
+#' \emph{Note}. x1 to x6 are consecutive data points of the primary outcome measure. x = Available data; . = Missing data. \strong{N} represent the pregain session for each "pattern".
 #' @param data A dataset in wide format with an id variable and the sudden gains variables.
 #' @param id_var_name String, specifying the name of the ID variable. Each row should have a unique value.
 #' @param sg_var_list Vector, specifying the variable names of each measurement point sequentially.
@@ -62,7 +73,7 @@ select_cases <- function(data, id_var_name, sg_var_list, method = c("pattern", "
 
     if (method == "pattern") {
 
-      base::message("The method 'pattern' was used to select cases.\nSee https://github.com/milanwiedemann/suddengains for more information.")
+      base::message("The method 'pattern' was used to select cases.\nSee help('select_cases') for more information.")
 
       # Return matrix indicating whether values are missing (FALSE) or available (TRUE)
       data_pattern <- !is.na(data_select[ , 2:(length(sg_var_list) + 1)])
@@ -85,7 +96,7 @@ select_cases <- function(data, id_var_name, sg_var_list, method = c("pattern", "
 
       } else if (method == "min_sess") {
 
-        base::message("The method 'min_sess' was used to select cases.\nSee https://github.com/milanwiedemann/suddengains for more information.")
+        base::message("The method 'min_sess' was used to select cases.")
 
         if (!is.numeric(min_sess_num)) {
             stop("`min_sess_num` argument should be numeric", call. = FALSE)
